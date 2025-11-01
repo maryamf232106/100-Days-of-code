134997cb3d981ac2b432e026b590163e3c1f5d02
maryamf232106 <maryamf232106@gmail.com>
2025-11-01 23:54:54 +0530

    Create day38.c

---
+++ day38.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int a[r][c], b[r][c], sum[r][c];
+
+    // Read first matrix
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    // Read second matrix
+    scanf("%d %d", &r, &c); // read same size again
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &b[i][j]);
+
+    // Add matrices
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
