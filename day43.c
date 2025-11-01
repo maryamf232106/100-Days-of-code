cbac663245fff8adc97acbe7086c5c6f850d2c5f
maryamf232106 <maryamf232106@gmail.com>
2025-11-01 23:28:51 +0530

    Create day43.c

---
+++ day43.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int len, i;
+
+    printf("Enter a string: ");
+    scanf("%s", str); // reads a single word (no spaces)
+
+    len = strlen(str);
+
+    printf("Reversed string: ");
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
