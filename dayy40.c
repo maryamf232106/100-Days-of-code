9b3c7d99fb2327c6940abbf25d170e51b44585cc
maryamf232106 <maryamf232106@gmail.com>
2025-11-01 23:42:48 +0530

    Create dayy40.c

---
+++ dayy40.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+
+    // Input dimensions
+    printf("Enter rows and columns for first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns for second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    // Check if multiplication is possible
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible.\n");
+        return 0;
+    }
+
+    int a[r1][c1], b[r2][c2], result[r1][c2];
+
+    // Input first matrix
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Initialize result matrix
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            result[i][j] = 0;
+        }
+    }
+
+    // Matrix multiplication logic
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    // Output result
+    printf("Resultant Matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
