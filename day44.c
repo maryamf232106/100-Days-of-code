4112baa6b8f175828d5b97584c1c7792e8cd2988
maryamf232106 <maryamf232106@gmail.com>
2025-11-01 23:19:59 +0530

    Create day44.c

---
+++ day44.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); // read full line (including spaces)
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            spaces++;
+        else if (isdigit(str[i]))
+            digits++;
+        else if (!isalpha(str[i]) && str[i] != '\n')
+            special++;
+    }
+
+    printf("Spaces=%d, Digits=%d, Special=%d\n", spaces, digits, special);
+    return 0;
+}
