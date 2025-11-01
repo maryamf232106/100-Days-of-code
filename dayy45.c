7e9d769dde3d0993bc053406b1fc9ce25300411f
maryamf232106 <maryamf232106@gmail.com>
2025-11-01 23:11:26 +0530

    Create dayy45.c

---
+++ dayy45.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (int i = 0; i < strlen(str); i++) {
+        if (islower(str[i]))
+            str[i] = toupper(str[i]);
+        else if (isupper(str[i]))
+            str[i] = tolower(str[i]);
+    }
+
+    printf("Output: %s\n", str);
+    return 0;
+}
