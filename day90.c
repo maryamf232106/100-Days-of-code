3c078e7cd37b695afee0fa1d5efffaa4095ff5c2
Maryam Fatima <maryamf232106@gmail.com>
2025-11-27 15:40:58 +0530

    Create day90.c

---
+++ day90.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Gender { MALE, FEMALE, OTHER };
+
+struct Person {
+    enum Gender gender;
+};
+
+int main() {
+    char input[20];
+    struct Person p;
+
+    scanf("%s", input);
+
+    if(strcmp(input, "MALE") == 0)
+        p.gender = MALE;
+    else if(strcmp(input, "FEMALE") == 0)
+        p.gender = FEMALE;
+    else
+        p.gender = OTHER;
+
+    switch(p.gender) {
+        case MALE: 
+            printf("Male\n");
+            break;
+        case FEMALE: 
+            printf("Female\n");
+            break;
+        case OTHER: 
+            printf("Other\n");
+            break;
+    }
+
+    return 0;
+}
